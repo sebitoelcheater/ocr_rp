acd3c41de899d8e8aae9d422521e2183a2cd060c
maitegm <maite.gonzalez.mendoza@gmail.com>
2014-08-26 16:19:03 -0400

    EightCC22.m
    añadido para celdas de 2*2
    (implementado para caso general x*y, para otras grillas cambiar
    variables cx y cy)

---
+++ EightCC22.m
@@ -0,0 +1,95 @@
+classdef EightCC22 < Concavity
+    %Implementado sólo para fotos cuadradas.
+    
+    properties
+    end
+    
+%     row = [];
+%     for subr = r:c
+%         subc = c - (subr-r);
+%         row(end+1) = binarized(subr, subc);
+%     end
+%     row
+    
+    methods
+        function [descriptor, concavity_matrix] = describe(self, picture)
+            binarized = picture.binarized;
+            [nrows, ncols] = size(binarized);
+            
+            concavity_matrix = zeros(nrows, ncols);
+            ndiagonals = ncols+nrows-1;
+            
+            % de sur-oeste hacia nor-este
+            for i = 1:ndiagonals
+                r = min(i, nrows);
+                c = max(i-nrows+1, 1);
+                for subc = c:r
+                    subr = r - (subc-c);
+                    if binarized(subr, subc) == 1
+                        concavity_matrix(subr, subc) = concavity_matrix(subr, subc) + 1;
+                    else
+                        break;
+                    end
+                end
+            end
+            
+            % de nor-este hacia sur-oeste
+            for i = 1:ndiagonals
+                c = min(i, ncols);
+                r = max(i-ncols+1, 1);
+                for subr = r:c
+                    subc = c - (subr-r);
+                    if binarized(subr, subc) == 1
+                        concavity_matrix(subr, subc) = concavity_matrix(subr, subc) + 2;
+                    else
+                        break;
+                    end
+                end
+            end
+            
+            % de sur-este hacia nor-oeste
+            for i = 1:ndiagonals
+                c = min(i, ncols);
+                r = min(ndiagonals-i+1, ncols);
+                for subr = r:-1:max(r-c+1,1)
+                    subc = c-(r-subr);
+                    if binarized(subr, subc) == 1
+                        concavity_matrix(subr, subc) = concavity_matrix(subr, subc) + 4;
+                    else
+                        break;
+                    end
+                end
+            end
+            
+            % de nor-oeste hacia sur-este
+            for i = 1:ndiagonals
+                c = min(i, ncols);
+                r = min(ndiagonals-i+1, ncols);
+                for subr = max(r-c+1,1):r
+                    subc = c-(r-subr);
+                    if binarized(subr, subc) == 1
+                        concavity_matrix(subr, subc) = concavity_matrix(subr, subc) + 8;
+                    else
+                        break;
+                    end
+                end
+            end
+            
+            cx = 2;
+            cy = 2;
+            descriptor = zeros(1,cx*cy*16);
+            k = 0;
+            for x = 1:cx
+                for y = 1:cy 
+                    for i = 1:16
+                        descriptor(1,i+(k*16)) = sum(sum(concavity_matrix == i-1));
+                    end
+                k = k+1;
+                end
+            end
+            
+            
+        end
+    end
+end
+
